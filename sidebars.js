8643630d07cf50f83c574de8f75441d71eeb0a20
onenox <onenoxti@gmail.com>
2024-04-08 17:43:58 +0300

    + Вхідні SMS

--- sidebars.js
+++ sidebars.js
@@ -12,7 +12,93 @@ let instruction_list = [
   }
 ]
 
-function client(){
+function client_uk(){
+  instruction_list.push(
+    {
+      type: 'category',
+      label: 'Send SMS',
+      items: [
+        'instruction/client/send_sms/single_sms',
+        'instruction/client/send_sms/bulk_sms',
+        'instruction/client/send_sms/voice_mailing',
+        'instruction/client/send_sms/viber',
+        //'instruction/client/send_sms/rcs_android',
+        'instruction/client/send_sms/sender_id',
+        'instruction/client/send_sms/templates',
+        'instruction/client/send_sms/planner'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Reports',
+      items: [
+        'instruction/client/reports/single_sms_reports',
+        'instruction/client/reports/bulk_sms_reports',
+        'instruction/client/reports/api_reports',
+        //'instruction/client/reports/base_cleaning_reports'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Finances',
+      items: [
+        'instruction/client/finances/payment',
+        'instruction/client/finances/balance',
+        'instruction/client/finances/documents',
+        'instruction/client/finances/transfer',
+        'instruction/client/finances/autopay'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Statistics',
+      items: [
+        'instruction/client/statistics/days',
+        'instruction/client/statistics/operators',
+        'instruction/client/statistics/sender_id_statistics',
+        'instruction/client/statistics/viber_sender_id',
+        'instruction/client/statistics/countries'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Incoming SMS',
+      items: [
+        'instruction/client/incoming_sms/received_sms',
+        'instruction/client/incoming_sms/tariffs'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Address Book',
+      items: [
+        'instruction/client/address_book/recipients',
+        'instruction/client/address_book/black_list'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'HLR',
+      items: [
+        'instruction/client/hlr/clear_base',
+        'instruction/client/hlr/hlr'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Settings',
+      items: [
+        'instruction/client/settings/profile',
+        'instruction/client/settings/api_settings',
+        'instruction/client/settings/sender_id_settings',
+        'instruction/client/settings/templates_sms',
+        'instruction/client/settings/templates_viber'
+      ]
+    }
+  )
+}
+
+function client_en(){
   instruction_list.push(
     {
       type: 'category',
@@ -220,6 +306,14 @@ function client_admin(){
         },
         {
           type: 'category',
+          label: 'Incoming SMS',
+          items: [
+            'instruction/client/incoming_sms/received_sms',
+            'instruction/client/incoming_sms/tariffs'
+          ]
+        },
+        {
+          type: 'category',
           label: 'Address Book',
           items: [
             'instruction/client/address_book/recipients',
@@ -327,14 +421,20 @@ function client_admin(){
   )
 }
 
+
 if (process.env.PSP_DOCS_DOMAIN == 'sms-doc.pages.dev' || process.env.PSP_DOCS_DOMAIN == 'partner.alphasms.ua' || process.env.PSP_DOCS_DOMAIN == 'partner.interconnect.solutions')
 {
   client_admin();
 }
 
-if (process.env.PSP_DOCS_DOMAIN == 'docs.alphasms.net' || process.env.PSP_DOCS_DOMAIN == 'docs.alphasms.ua' || process.env.PSP_DOCS_DOMAIN == 'docs.interconnect.solutions')
+if (process.env.PSP_DOCS_DOMAIN == 'docs.alphasms.net' || process.env.PSP_DOCS_DOMAIN == 'docs.interconnect.solutions')
 {
-  client();
+  client_en();
+}
+
+if (process.env.PSP_DOCS_DOMAIN == 'docs.alphasms.ua')
+{
+  client_uk();
 }
 
 module.exports = {
